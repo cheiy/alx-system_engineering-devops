abbc27a4a7c5522d39cb4b12e5cc61486edda528
cheiy <gschege@gmail.com>
2023-04-25 21:17:38 +0000

    Puppet - Nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,79 @@
-# Manifest installs Nginx web server and enables redirection
-package { 'Nginx':
-  ensure  => 'installed',
-  require => Package['nginx'],
+#Add ppa repo for stable version of nginx
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-service {'nginx':
-  ensure  => 'running',
-  require => file_line['perform a redirection'],
+#Update software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-file { '/var/www/html/index.nginx-debian.html':
-  ensure  => 'present',
-  content => 'Hello World!',
-  require => Package['nginx']
+# Install nginx
+package { 'nginx':
+  ensure     => 'installed',
 }
 
-file_line { 'Redirection':
-  ensure  => 'present',
+# Allow HTTP
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
+}
+
+# Update folder rights
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# Create index file
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
+
+# Create index file
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
+
+#Add direction & error pages
+file { 'Nginx default config file':
+  ensure  => file,
   path    => '/etc/nginx/sites-enabled/default',
-  line    => 'rewrite ^/redirect_me$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  after   => 'root /var/www/html;',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
+        }
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
+}
+",
+}
+#Reload nginx
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+#start nginx
+service { 'nginx':
+  ensure  => running,
   require => Package['nginx'],
-  notify  => Service['nginx'],
 }
