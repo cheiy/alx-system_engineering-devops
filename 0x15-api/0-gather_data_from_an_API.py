b493a1e9418a1a24bea09ae58e3b167a5d65d10c
cheiy <gschege@gmail.com>
2023-05-22 19:14:27 +0000

    if __name == __main__

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,20 +7,21 @@ TODO list progress given their employee ID
 import requests
 from sys import argv
 
-url = 'https://jsonplaceholder.typicode.com/users/'
-x = requests.get(url+argv[1])
-name = x.json()['name']
-completed = []
-complete = 0
-incomplete = 0
-todos = requests.get(url+argv[1]+'/todos')
-for item in todos.json():
-    if item['completed'] is True:
-        complete += 1
-        completed.append(item['title'])
-    elif item['completed'] is False:
-        incomplete += 1
-total = complete + incomplete
-print("Employee {} is done with tasks({}/{}):".format(name, complete, total))
-for task in completed:
-    print('     '+task)
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    x = requests.get(url+argv[1])
+    name = x.json()['name']
+    completed = []
+    complete = 0
+    incomplete = 0
+    todos = requests.get(url+argv[1]+'/todos')
+    for item in todos.json():
+        if item['completed'] is True:
+            complete += 1
+            completed.append(item['title'])
+        elif item['completed'] is False:
+            incomplete += 1
+    total = complete + incomplete
+    print("Employee {} is done with tasks({}/{}):".format(name, complete, total))
+    for task in completed:
+        print('\t '+task)
