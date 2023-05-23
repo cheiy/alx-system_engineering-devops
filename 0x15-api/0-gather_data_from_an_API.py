9182e232e3f7833f10cbaef71f04265716b41598
cheiy <gschege@gmail.com>
2023-05-23 01:33:29 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,27 +4,32 @@ This Python script uses specified REST API to return info about an employee's
 TODO list progress given their employee ID
 '''
 
+
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
-    users = 'https://jsonplaceholder.typicode.com/users/'
-    todos = 'https://jsonplaceholder.typicode.com/todos/'
-    x = requests.get(users+argv[1])
-    name = x.json()['name']
-    completed = []
+
+    session = requests.Session()
+    EmpId = argv[1]
+    todos = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(EmpId)
+    names = 'https://jsonplaceholder.typicode.com/users/{}'.format(EmpId)
+
+    employee = session.get(todos)
+    employeeName = session.get(names)
+
+    json_request = employee.json()
+    name = employeeName.json()['name']
     complete = 0
-    incomplete = 0
-    to_dos = requests.get(todos)
-    for item in to_dos.json():
-        if item['userId'] == int(argv[1]):
-            if item['completed'] is True:
-                complete += 1
-                completed.append(item['title'])
-            elif item['completed'] is False:
-                incomplete += 1
-    total = complete + incomplete
-    print("Employee {} is done with tasks({}/{}):".format(name, complete, total
-                                                          ))
-    for task in completed:
-        print('\t'+" "+task)
+
+    for tasks in json_request:
+        if tasks['completed']:
+            complete += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, complete, len(json_request)))
+
+    for complete in json_request:
+        if complete['completed']:
+            print("\t " + complete.get('title'))
