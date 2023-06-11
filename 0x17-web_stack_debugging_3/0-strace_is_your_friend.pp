2a8e3fe822692d55db557bf7246c6b6f66c4186d
cheiy <gschege@gmail.com>
2023-06-11 18:57:08 +0000

    Error fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,6 @@
 # This manifest file fixes wordpress settings
 
-file { '/var/www/html/wp-settings.php':
-  ensure => present,
-}
-->file_line { 'wp-locale':
-  ensure             => present,
-  path               => '/var/www/html/wp-settings.php',
-  line               => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match              => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );",
-  append_on_no_match => false,
+exec { 'wp-locale':
+  provider => 'shell',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
