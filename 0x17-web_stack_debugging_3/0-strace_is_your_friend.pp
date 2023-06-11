6662ef87cbd7998ea634a9b0e36f47065629e8cf
cheiy <gschege@gmail.com>
2023-06-11 18:52:15 +0000

    append_on_no_match = false

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,8 +3,9 @@ file { '/var/www/html/wp-settings.php':
   ensure => present,
 }
 ->file_line { 'wp-locale':
-  ensure => present,
-  path   => '/var/www/html/wp-settings.php',
-  line   => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match  => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );",
+  ensure             => present,
+  path               => '/var/www/html/wp-settings.php',
+  line               => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
+  match              => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );",
+  append_on_no_match => false,
 }
